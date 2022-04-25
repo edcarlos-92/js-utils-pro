01132ef8da9dad11b3f1faebb7f23fefd7078b44
edcarlos-92 <edcartech@gmail.com>
2022-04-25 22:12:01 +0000

    Module Export

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-export function closeStringSpace(string) {
+module.exports = function closeStringSpace(string) {
+  //export function closeStringSpace(string) {
   if (typeof string !== 'string')
     throw new TypeError('closeStringSpace wants a string!');
   return string.replace(/\s/g, '');
-}
+};
 
 // module.exports = function closeStringSpace(string) {
 //   if (typeof string !== 'string')
